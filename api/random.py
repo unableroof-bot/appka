ede0c54821267cfcea0a0033b6c1e7d8e69cd733
Dmitry <unableroof@gmail.com>
2025-12-30 10:00:33 +0400

    test

--- api/random.py
+++
@@ -1,9 +0,0 @@
-import random
-from .participants import participants
-
-def pick_winner():
-    if not participants:
-        return None
-    winner = random.choice(participants)
-    participants.clear()
-    return winner
